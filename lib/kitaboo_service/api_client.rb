65f9c1117de72db79c7ab9362872bb1b7f743dc1
João Ferreira <jpedaf@gmail.com>
2024-12-18 18:14:47 -0300

    Implement methods of Kitaboo Service API Client

--- lib/kitaboo_service/api_client.rb
+++ lib/kitaboo_service/api_client.rb
@@ -18,6 +18,42 @@ module KitabooService
     end
   end
 
+  def remove_kitaboo_user_by_email(email)
+    url = URI(@kitaboo_routes.notification_route)
+    https = Net::HTTP.new(url.host, url.port)
+    https.use_ssl = true
+    request = Net::HTTP::Post.new(url)
+    request['Authorization'] = "Bearer #{@kitaboo_auth_token}"
+    request['Content-Type'] = 'application/x-www-form-urlencoded'
+    request.body = "userID=#{email}&userDeleted=1"
+    KitabooResponse.new(https.request(request))
+  end
+
+  def remove_order(order_number)
+    url = URI("#{@kitaboo_routes.orders_route}?orderNo=#{order_number}")
+    https = Net::HTTP.new(url.host, url.port)
+    https.use_ssl = true
+    request = Net::HTTP::Delete.new(url)
+    request["Authorization"] = "Bearer #{@kitaboo_auth_token}"
+    request["Content-Type"] = "application/x-www-form-urlencoded"
+    KitabooResponse.new(https.request(request))
+  end
+
+  def create_order(order_number, course_code, student_email, password)
+    url = URI(@kitaboo_routes.orders_route)
+    https = Net::HTTP.new(url.host, url.port)
+    https.use_ssl = true
+    request = Net::HTTP::Post.new(url.path)
+    request["Authorization"] = "Bearer #{@kitaboo_auth_token}"
+    request["Content-Type"] = "application/x-www-form-urlencoded"
+    request.body = "orderNo=#{order_number}&bookID=#{course_code}&userID=#{student_email}&firstName=#{student_email}&lastName=#{student_email}&userName=#{student_email}&password=#{password}&email=#{student_email}"
+    KitabooResponse.new(https.request(request))
+  end
+
+  def is_production_environment
+    @is_kitaboo_production
+  end
+
   private
 
   def authenticate_production_environment
